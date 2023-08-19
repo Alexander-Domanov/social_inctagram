e172a00511a9ae2f55fa190884a8e6667131e81e
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-08-19 22:07:53 +0300

    ref - test build

--- src/modules/create-post-module/components/photo-filters-editor/FilterImage.tsx
+++ src/modules/create-post-module/components/photo-filters-editor/FilterImage.tsx
@@ -1,6 +1,6 @@
 import React, { FC, memo } from 'react'
 
-import { IPhoto, useImageSelector } from '@/store/storeSelectorPhoto'
+import { IPhoto } from '@/store/storeSelectorPhoto'
 
 interface IFiltersEditor {
   srs: IPhoto
