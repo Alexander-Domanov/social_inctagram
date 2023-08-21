727e9f47533084476374b16fc8c230845ab7452c
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-08-21 23:35:50 +0300

    css(adaptive) - fixed margins in settings profile

--- src/components/account/account-layout/AccountLayout.tsx
+++ src/components/account/account-layout/AccountLayout.tsx
@@ -4,9 +4,5 @@ export const AccountLayout: FC<{ children: ReactNode; className?: string }> = ({
   children,
   className,
 }) => {
-  return (
-    <div className={`max-w-[750px] w-full pt-[24px] pl-[24px] pb-[24px]'${className}`}>
-      {children}
-    </div>
-  )
+  return <div className={`max-w-[750px] w-full'${className}`}>{children}</div>
 }
