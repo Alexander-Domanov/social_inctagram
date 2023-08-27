9f9be3db29ba6dc2af87fe8022cfe9938ebbc04d
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-08-27 21:24:44 +0300

    css(settings-edit-profile) - added a button on the profile page

--- src/modules/my-profile-modules/settings-edit-profile-module/SettingsProfile.tsx
+++ src/modules/my-profile-modules/settings-edit-profile-module/SettingsProfile.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react'
 
+import Link from 'next/link'
+
 import { settings_profile_tabs, useLocalStorage } from '@/common'
 import { TabsTitle } from '@/components/account'
+import { ArrowBack } from '@/modules/my-profile-modules/settings-edit-profile-module/utils/ArrowBack'
+import { routes } from '@/routing/router'
 
 export const SettingsProfile = () => {
   const [storedTabsLabel, setStoredTabsLabel] = useLocalStorage(
@@ -27,6 +31,10 @@ export const SettingsProfile = () => {
 
   return (
     <div className="relative w-full">
+      <Link href={routes.sideBar.profile} className="flex pb-5 w-full hidingElementMoreMobile">
+        <ArrowBack />
+        <span className="text-lg m-auto font-bold leading-6 text-light-100">Profile Settings</span>
+      </Link>
       <TabsTitle tabs={settings_profile_tabs} setActiveTab={onChangeTab} activeTab={activeTab} />
       {tabsLayout}
     </div>
