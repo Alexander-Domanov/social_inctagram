fd78d9a9fd3834e252197a3f12d7a352598a76e5
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-08-28 01:28:58 +0300

    feat(user-profile-page) - added skeletons for pages

--- src/modules/my-profile-modules/profile-page-module/profile-page/ProfilePage.tsx
+++ src/modules/my-profile-modules/profile-page-module/profile-page/ProfilePage.tsx
@@ -9,7 +9,10 @@ import {
   InfoAboutProfilePage,
 } from '@/components/info-about-profile-page'
 import { useTranslation } from '@/components/translation'
-import { useReplacePathnameQueryEffect } from '@/modules/my-profile-modules/profile-page-module'
+import {
+  SkeletonProfilePage,
+  useReplacePathnameQueryEffect,
+} from '@/modules/my-profile-modules/profile-page-module'
 import { useGetProfile } from '@/modules/my-profile-modules/settings-edit-profile-module'
 import { StateModalFollowingFollowersType } from '@/types'
 import { Avatar, GlobalButton } from '@/ui'
@@ -17,7 +20,7 @@ import { LatestPosts } from 'src/modules/post-modules/latest-posts-module'
 
 export const ProfilePage = () => {
   const { push } = useRouter()
-  const { profileData, profileAvatar } = useGetProfile()
+  const { profileData, profileAvatar, isFetchingProfileData } = useGetProfile()
   const { t } = useTranslation()
   const { onCloseClick, modalOpen, setModalOpen } =
     useOpenCloseModal<StateModalFollowingFollowersType>({})
@@ -34,41 +37,49 @@ export const ProfilePage = () => {
   return (
     <div className="flex w-full">
       <main className="grow">
-        <div className="flex xsm:gap-0 sm:gap-3 sm:items-center text-light-100 gap-9">
-          <Avatar
-            src={avatar}
-            alt={'photo'}
-            className={
-              'xsm:w-[72px] xsm:mr-7 xsm:h-[72px] sm:w-[120px] sm:h-[120px] md:w-[140px] md:h-[140px]'
-            }
-          />
-          <div className="flex w-full flex-col gap-5">
-            <div className="flex sm:gap-5 md:gap-5 lg:gap-5 flex-wrap justify-between">
-              <div className="font-bold break-all xsm:hidden sm:hidden md:hidden">{userName}</div>
-              <GlobalButton
-                className={'xsm:hidden text-base bg-dark-300 font-semibold'}
-                type={'button'}
-                variant={'gray'}
-                callback={onRedirectToSetting}
-              >
-                <span className="font-semibold text-light-100 text-base leading-6">
-                  {t.profile.profilePage.buttonProfileSettings}
-                </span>
-              </GlobalButton>
+        {!isFetchingProfileData ? (
+          <>
+            <div className="flex xsm:gap-0 sm:gap-3 sm:items-center text-light-100 gap-9">
+              <Avatar
+                src={avatar}
+                alt={'photo'}
+                className={
+                  'xsm:w-[72px] xsm:mr-7 xsm:h-[72px] sm:w-[120px] sm:h-[120px] md:w-[140px] md:h-[140px]'
+                }
+              />
+              <div className="flex w-full flex-col gap-5">
+                <div className="flex sm:gap-5 md:gap-5 lg:gap-5 flex-wrap justify-between">
+                  <div className="font-bold break-all xsm:hidden sm:hidden md:hidden">
+                    {userName}
+                  </div>
+                  <GlobalButton
+                    className={'xsm:hidden text-base bg-dark-300 font-semibold'}
+                    type={'button'}
+                    variant={'gray'}
+                    callback={onRedirectToSetting}
+                  >
+                    <span className="font-semibold text-light-100 text-base leading-6">
+                      {t.profile.profilePage.buttonProfileSettings}
+                    </span>
+                  </GlobalButton>
+                </div>
+                <InfoAboutProfilePage
+                  t={t}
+                  aboutMe={aboutMe}
+                  publications={publications}
+                  following={followings}
+                  followers={followers}
+                  setModalOpen={setModalOpen}
+                />
+              </div>
             </div>
-            <InfoAboutProfilePage
-              t={t}
-              aboutMe={aboutMe}
-              publications={publications}
-              following={followings}
-              followers={followers}
-              setModalOpen={setModalOpen}
-            />
-          </div>
-        </div>
-        <ModalManagerFollowingFollowers isModalOpen={modalOpen} onClose={onCloseClick} />
-        <DuplicateUserNameDescription userName={userName} aboutMe={aboutMe} />
-        <LatestPosts />
+            <ModalManagerFollowingFollowers isModalOpen={modalOpen} onClose={onCloseClick} />
+            <DuplicateUserNameDescription userName={userName} aboutMe={aboutMe} />
+            <LatestPosts />
+          </>
+        ) : (
+          <SkeletonProfilePage />
+        )}
       </main>
     </div>
   )
---
+++ src/modules/my-profile-modules/profile-page-module/profile-page/SkeletonDuplicateUserNameDescriotion.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Skeleton } from '@/ui/skeletons/Skeleton'
+
+export const SkeletonDuplicateUserNameDescription = () => {
+  return (
+    <>
+      <div className="sm:mt-4 xl:hidden lg:hidden exl:hidden mt-2 flex flex-col text-light-100 w-full">
+        <Skeleton classes={'w-[115px] h-[36px]'} />
+        <div className="w-full mt-3">
+          <Skeleton classes={'w-[720px] h-[73px]'} />
+        </div>
+      </div>
+    </>
+  )
+}
