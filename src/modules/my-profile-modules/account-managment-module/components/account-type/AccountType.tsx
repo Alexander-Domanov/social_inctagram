3ddb82840de62f394b7661177e82b5d5162fcff0
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-10-15 20:38:25 +0300

    ref(account-management) - eslint

--- src/modules/my-profile-modules/account-managment-module/components/account-type/AccountType.tsx
+++ src/modules/my-profile-modules/account-managment-module/components/account-type/AccountType.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useTranslation } from '@/components/translation'
 import { Radio } from '@/ui/radio/Radio'
